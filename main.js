0893524326a59643c26a5bcab2c5ecec91ab2b16
neil <jacoborosseau@gmail.com>
2023-02-03 09:19:03 -0500

    segundo cambio

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import { BreakingBad } from './src/breakingbad/breakingbad-app';
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="./vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
